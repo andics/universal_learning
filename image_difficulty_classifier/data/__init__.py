164d153903b361dc30822acb09e7288892271c60
andics <andics1243@gmail.com>
2025-08-15 18:09:49 -0400

    Update

--- image_difficulty_classifier/data/__init__.py
+++ image_difficulty_classifier/data/__init__.py
@@ -1,8 +1,9 @@
-from .imagenet_bins import ImageNetDifficultyBinDataset, default_transforms
+from .imagenet_bins import ImageNetDifficultyBinDataset, default_transforms, indices_to_bins
 
 __all__ = [
     "ImageNetDifficultyBinDataset",
     "default_transforms",
+    "indices_to_bins",
 ]
 
 
